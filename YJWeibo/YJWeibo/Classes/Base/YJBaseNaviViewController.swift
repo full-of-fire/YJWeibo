11193d1cf6641506f94a2c1dc8aed1aa9c566808
yong <yong@qq.com>
2016-08-20 10:44:30 +0800

    完成基本框架的搭建

--- YJWeibo/YJWeibo/Classes/Base/YJBaseNaviViewController.swift
+++ YJWeibo/YJWeibo/Classes/Base/YJBaseNaviViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
+
+
 class YJBaseNaviViewController: UINavigationController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
+        
         // Do any additional setup after loading the view.
     }
 
--- YJWeibo/YJWeibo/Classes/Base/YJBaseTabBarViewController.swift
+++ YJWeibo/YJWeibo/Classes/Base/YJBaseTabBarViewController.swift
@@ -12,24 +12,95 @@ class YJBaseTabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+        // 解析本地json
+        let jsonPath = NSBundle.mainBundle().pathForResource("localSettings.json", ofType: nil)
+        
+        if let resultPath = jsonPath {
+            
+            print(resultPath)
+            
+             //转为data
+            let fileData = NSData.init(contentsOfFile: resultPath)
+            
+            if let realData = fileData  {
+                
+                //在这里序列化，序列化可能会出现，抛异常
+                
+                do {
+                
+                    // 这里正常解析
+                    let jsonArr = try NSJSONSerialization.JSONObjectWithData(realData, options: .MutableContainers)
+                    
+                    //遍历数组,这里需要注意因为json返回的是Anyobject对象，需要将其转为数组
+                    for dict in jsonArr as![[String:String]] {
+                    
+                        addChildViewController(dict["vcName"]!, title: dict["title"]!, imageName: dict["imageName"]!)
+                    }
+                    
+                    print("解析成功")
+                 
+                    
+                }
+                catch {
+                
+                    print(error)
+                    // 异常处理,如果解析json失败，就自己创建
+                    //创建首页
+                    addChildViewController("YJMainViewController", title: "首页", imageName:"tabbar_home")
+                    
+                    //消息
+                    addChildViewController("YJMessageViewController", title: "消息", imageName:"tabbar_message_center")
+                    
+                    //发现
+                    addChildViewController("YJAttentionViewController", title: "发现", imageName:"tabbar_discover")
+                    
+                    //我的
+                    addChildViewController("YJMineViewController", title: "我的", imageName:"tabbar_profile")
+                }
+                
+                
+            }
+            
+        }
+        
+        
+        
+        
         
         //设置tabbar
         tabBar.tintColor = UIColor.orangeColor()
-        //创建首页
-       addChildViewController("YJMainViewController", title: "首页", imageName:"tabbar_home")
-        
-        //消息
-        addChildViewController("YJMessageViewController", title: "消息", imageName:"tabbar_message_center")
-        
-        //发现
-         addChildViewController("YJAttentionViewController", title: "发现", imageName:"tabbar_discover")
-        
-        //我的
-         addChildViewController("YJMainViewController", title: "我的", imageName:"tabbar_profile")
+     
        
 
         
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        
+        // 在这里添加按钮
+        let buttonW = UIScreen.mainScreen().bounds.width / CGFloat(childViewControllers.count)
+        
+        composeButton.frame = CGRectMake(2*buttonW, 0, buttonW, 49)
+        
+        tabBar.addSubview(composeButton)
+        
+        
+        
+        
+    }
+    
+    //MARK: 按钮点击事件
+    func composeClick() {
+        
+        
+        print("我被点击了")
+    }
+    
+    
     
     // MARK: 添加子控制
     private func  addChildViewController(childController: UIViewController ,title:String, imageName:String) {
@@ -47,6 +118,8 @@ class YJBaseTabBarViewController: UITabBarController {
         
         
     }
+    
+    
     
     // MARK: 通过字符串动态加载控制
     
@@ -72,6 +145,32 @@ class YJBaseTabBarViewController: UITabBarController {
         
         
     }
+    
+    
+    
+    //MARK: 懒加载一个按钮
+    private lazy var composeButton:UIButton = {
+    
+       let btn = UIButton()
+
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: .Normal)
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: .Selected)
+        
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: .Normal)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: .Selected)
+        
+        
+//         btn.addTarget(self, action: "composeClick", forControlEvents: .TouchUpInside)
+        btn.addTarget(self, action: #selector(YJBaseTabBarViewController.composeClick), forControlEvents: .TouchUpInside)
+        
+
+        
+        return btn
+        
+    }()
+    
+   
+    
 
     
 }
